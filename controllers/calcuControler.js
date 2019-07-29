67031df4c5b5a094d7553b062cd696fc7f9d5668
Hugo Costilla <hugo.costillah@gmail.com>
2019-07-29 16:01:56 -0500

    testing routes and controllers

---
+++ controllers/calcuControler.js
@@ -0,0 +1,26 @@
+// arithmetic functions
+
+const Maths = {};
+
+// maths.add = ()=>{
+
+// }
+
+// maths.substract = ()=>{
+
+// }
+
+// maths.multi = ()=>{
+
+// }
+
+// maths.division = ()=>{
+
+// }
+
+Maths.greets = ()=>{
+    console.log("Hola desde controladores");
+}
+
+module.exports = Maths;
+
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,38 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', 
-  { 
-    title: 'Express',
-    message: "Probando Express"
-  });
+  res.status(200).json({message: 'escuchando'})
 });
+
+router.get('/users/hola', function(req, res, next) {
+  res.status(200).json(
+    {
+      message: "probando peticiones en postman"
+    }
+  );
+});
+
+// router.get('/users/b', (req, res, next)=>{
+//   console.log('la respuesta sera enviada por la siguiente funcion');
+//   next();
+// }, (req, res)=>{
+//   res.send('este es un mensaje enviado desde B');
+// });
+
+// var cb0 = function (req, res, next) {
+//   console.log('CB0');
+//   next();
+// }
+
+// var cb1 = function (req, res, next) {
+//   console.log('CB1');
+//   next();
+// }
+
+// var cb2 = function (req, res) {
+//   res.send('Hello from C!');
+// }
+
+// router.get('/users/c', [cb0, cb1, cb2]);
 
 module.exports = router;
